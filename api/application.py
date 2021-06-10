4ce288535390867447a893a36c3bc97b1d2db21e
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-06-10 18:40:40 +0100

    Added simple drinks API project with Create/Read/Delete, missing the Update method.

---
+++ api/application.py
@@ -0,0 +1,63 @@
+from flask import Flask, request
+from flask.json import jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# APP Configurations
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+# Model configuration
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+# API routes
+@app.route('/')
+def index():
+    return 'Hello!'
+
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+
+    return {"drinks": output}
+
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    # return jsonify({"name": drink.name, "description": drink.description})
+    return {"name": drink.name, "description": drink.description}
+
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'],
+                  description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drinks(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error": "not found!!!"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message": "drink deleted!"}
